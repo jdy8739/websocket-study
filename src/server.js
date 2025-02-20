179a7286c9430fe66b927b1e91a9cb91aac6e4c4
jdy8739 <jdy8739@naver.com>
2025-02-20 23:26:27 +0900

    chore: project set up with node.js and websocket

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import path from 'path';
+import http from 'http';
+import { fileURLToPath } from 'url';
+import { WebSocketServer } from "ws";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
+
+const server = http.createServer(app);
+
+const wss = new WebSocketServer({ server });
+
+server.listen(3000, () => console.log('hey'));
+
